d56957b88a9c929ddab9cf677d07d10fca3940c9
nastyanikitinteam <nastya@nikitinteam.com>
2023-08-01 16:51:57 +0200

    start tamplate

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode, FC, useState } from "react";
+import Head from "next/head";
+import Header from "components/Header/Header";
+import Footer from "components/Footer/Footer";
+
+import styles from "./layout.module.scss";
+
+interface IProps {
+  title: string;
+  children: ReactNode;
+}
+
+const Layout: FC<IProps> = ({ title = "Page", children }) => {
+  return (
+    <div className={styles["app"]}>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Header />
+      <div className={styles.container}>{children}</div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+import Layout from "../components/Layout/Layout";
+
+const Home: NextPage = () => {
+  return <Layout title="Home">Home</Layout>;
+};
+
+export default Home;
