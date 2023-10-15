5cf1a4ead940aeec467a72dd38654eabce099c88
nastyanikitinteam <nastya@nikitinteam.com>
2023-10-15 23:00:42 +0300

    edit chat

--- src/components/ProfilePage/Chat/Info/Block/Block.tsx
+++ src/components/ProfilePage/Chat/Info/Block/Block.tsx
@@ -1,6 +1,8 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styles from "./block.module.scss";
 import cn from "classnames";
+
+import DelIcon from "images/icons/delete.svg";
 
 interface IProps {
   id: number;
@@ -23,6 +25,18 @@ const Block: FC<IProps> = ({
   isActiveChatID,
   onClick,
 }) => {
+  const [leftPosition, setLeftPosition] = useState(0);
+
+  const handleTouchMove = (e) => {
+    const touchX = e.touches[0].clientX;
+    const deltaX = touchX - e.touches[0].clientX;
+
+    if (deltaX < 0) {
+      setLeftPosition(-60);
+    } else {
+      setLeftPosition(0);
+    }
+  };
   return (
     <div
       className={cn(
@@ -33,18 +47,24 @@ const Block: FC<IProps> = ({
         { [styles.isNew]: isNew }
       )}
       onClick={onClick}
+      onTouchMove={handleTouchMove}
     >
-      <div className={styles.avatar}>
-        <img src={avatar} alt="" />
+      <div className={styles.del}>
+        <DelIcon />
       </div>
-      <div className={styles.main}>
-        <div className={styles.top}>
-          <p className={styles.name}>{name}</p>
-          <p className={styles.time}>{messages[0].time}</p>
+      <div className={styles.block} style={{ left: `${leftPosition}px` }}>
+        <div className={styles.avatar}>
+          <img src={avatar} alt="" />
         </div>
-        <div className={styles.info}>
-          <h4 className={styles.title}>{title}</h4>
-          {isNew && <span className={styles.new}>{isNew}</span>}
+        <div className={styles.main}>
+          <div className={styles.top}>
+            <p className={styles.name}>{name}</p>
+            <p className={styles.time}>{messages[0].time}</p>
+          </div>
+          <div className={styles.info}>
+            <h4 className={styles.title}>{title}</h4>
+            {isNew && <span className={styles.new}>{isNew}</span>}
+          </div>
         </div>
       </div>
     </div>
