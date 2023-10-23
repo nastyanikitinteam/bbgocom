51a89b92754b4a4eb7fa777900306d5b2055ee8e
nastyanikitinteam <nastya@nikitinteam.com>
2023-10-23 10:16:03 +0300

    selectes ad - edit successful modal

--- src/components/Share/Copy/Copy.tsx
+++ src/components/Share/Copy/Copy.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState, useRef } from "react";
 import { useRouter } from "next/router";
 import styles from "./copy.module.scss";
+import cn from "classnames";
 
 import CopyIcon from "images/icons/copy.svg";
 
 const Copy = () => {
-  const [inputValue, setInputValue] = useState("https://bbgo.com/ad_id1234456");
+  // const [inputValue, setInputValue] = useState("https://bbgo.com/ad_id1234456");
+  const [isCopied, setIsCopied] = useState(false);
   const [currentFullUrl, setCurrentFullUrl] = useState("");
   const router = useRouter();
   const inputRef = useRef(null);
@@ -14,6 +16,7 @@ const Copy = () => {
     inputRef.current.select();
     document.execCommand("copy");
     window.getSelection().removeAllRanges();
+    setIsCopied(true);
   };
 
   useEffect(() => {
@@ -23,12 +26,12 @@ const Copy = () => {
   }, [router.asPath]);
 
   return (
-    <div className={styles.container}>
+    <div className={cn(styles.container, { [styles.copy]: isCopied })}>
       <input
         ref={inputRef}
         type="text"
         value={currentFullUrl}
-        onChange={(e) => setInputValue(e.target.value)}
+        // onChange={(e) => setInputValue(e.target.value)}
         readOnly
         className="default-input sm withoutIcon"
       />
